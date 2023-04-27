5cddc7ccf46054ef945249fae9cd8cf117a3364a
Austin Do <austinndo@gmail.com>
2023-04-27 16:48:00 -0700

    Added more notes, now working on what is rendered when a user goes to a path that has not been declared

--- pages/side_bar.py
+++ pages/side_bar.py
@@ -3,6 +3,8 @@ from dash import html
 import dash_bootstrap_components as dbc
 
 
+# dash.page_registry is accessed within the sidebar function. If dash.page_registry was not within a function the app wouldn't work because 'dash.page_registry wouldn't  be ready when the page loads'
+
 def sidebar():
     return html.Div(
         dbc.Nav(
