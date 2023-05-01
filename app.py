b55dcd27a892b213fc4eec5912e46a3eb373b3e0
Austin Do <austinndo@gmail.com>
2023-05-01 12:09:10 -0700

    Wrapping up with practice, ready to build first app! Added more notes for future reference

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import dash
 from dash import Dash, html,  dcc
 
+# In order to allow multi pages, set use_pages=True.
+# Dash looks for a pages folder but if you want to rename this you can use:
+## app = dash.Dash(__name__, use_pages=True, pages_folder="[name of folder]")
+
 app = Dash(__name__, use_pages=True)
 
 # In each of my pages the layout is defined using the variable 'layout'. This can also be done by useing a function called 'layout' (i.e. def layout(): )
@@ -21,3 +25,31 @@ app.layout = html.Div([
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+
+# See below to set up multiple pages all within one file instead of how we set up the pages folder:
+# from dash import Dash, html, dcc, callback
+# import dash
+
+# app = Dash(__name__, use_pages=True)
+
+# dash.register_page("home",  path='/', layout=html.Div('Home Page'))
+# dash.register_page("analytics", layout=html.Div('Analytics'))
+
+# app.layout = html.Div([
+#     html.Div(
+#         [
+#             html.Div(
+#                 dcc.Link(
+#                     f"{page['name']} - {page['path']}", href=page["relative_path"]
+#                 )
+#             )
+#             for page in dash.page_registry.values()
+#         ]
+#     ),
+#     dash.page_container,
+# ])
+
+
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
