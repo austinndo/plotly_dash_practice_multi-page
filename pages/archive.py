d1de88566a020a196ef20db06580e7fb77c09403
Austin Do <austinndo@gmail.com>
2023-05-01 11:42:19 -0700

    Update to two query string parameters

--- pages/archive.py
+++ pages/archive.py
@@ -4,13 +4,14 @@ from dash import html
 dash.register_page(__name__)
 
 
-def layout(report_id=None):
+def layout(report_id=None, department_id=None, **other_unknown_query_strings):
     return html.Div(
         children=[
             html.H1(children='This is our Archive page'),
 
             html.Div(children=f'''
-	        This is report: {report_id}.
+	        This is report: {report_id}.\n
+			This is department: {department_id}.
 	    '''),
 
         ])
